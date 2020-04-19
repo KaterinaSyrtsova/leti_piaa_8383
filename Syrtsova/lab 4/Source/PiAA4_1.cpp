b7724f1177bed65ca37613938da7c553bbd7376f
KaterinaSyrtsova <katerinasyrcova@gmail.com>
2020-04-19 17:30:11 +0300

    Syrtsova-lab4
    Вар. 2. Оптимизация по памяти: программа должна требовать O(m) памяти, где m - длина образца. Это возможно, если не учитвать память, в которой хранится строка поиска.

---
+++ Syrtsova/lab 4/Source/PiAA4_1.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector <int> prefix(string arr) {//префикс функция от подстроки
+	cout << endl << "Вычислим префикс функцию подстроки" << endl;
+	vector <int> p(arr.size());
+	size_t j = 0;
+	size_t i = 1;
+	p[0] = 0;
+	cout << "Первое вхождение символа\nПрефикс " << arr[0] << " = 0"<< endl;
+	while (i < arr.length()) {
+		if (arr[i] == arr[j]) {//если символы совпадают
+			cout << "Символ " << arr[i] << " совпадает с " << arr[j] << endl;
+			p[i] = j + 1;//увеличиваем префикс 
+			cout << "Префикс " << arr[i] << " = " << p[i] << endl;
+			i++;
+			j++;
+		}
+		else if (j == 0) {//еще не было совпадений
+			p[i] = 0;
+			cout << "Символ " << arr[i] << " не совпадает с " << arr[0] << endl;
+			cout << "Префикс " << arr[i] << " = " << p[i] << endl;
+			i++;
+		}
+		else {//символы не совпадают
+			cout << "Символ " << arr[i] << " не совпадает с " << arr[j] << endl;
+			j = p[j - 1];
+		}
+	}
+	cout << "Префикс функция строки ";
+	for (i = 0; i < arr.size(); i++) cout << p[i];
+	cout << endl;
+	return p;
+}
+
+vector <int> KMP(string form, string line) {
+	vector <int> res;
+	vector <int> p = prefix(form);//находим префикс
+	size_t i = 0;
+	size_t j = 0;
+	cout << endl << "Алгоритм KMP" << endl;
+	while (i < line.length()) {
+		if (line[i] == form[j]) {//если символы равны
+			cout << "Символ " << line[i] << " совпадает с " << form[j] << endl;
+			i++;
+			j++;
+			if (j == form.length()) {
+				res.push_back(i - form.length());
+			}
+		}
+		else if (j != 0) {
+			if (j!=form.length()) cout << "Символ " << line[i] << " не совпадает с " << form[j] << endl;
+			j = p[j - 1];
+			cout << "Вернемся к предыдущему символу префикса в подстроке" << endl;
+		}
+		else {
+			cout << "Символы не совпадают" << endl;
+			i++;
+		}
+	}
+	return res;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	string form;//подстрока
+	string line;//строка
+	getline(cin, form);
+	getline(cin, line);
+	vector <int> res = KMP(form, line);
+	if (res.size() == 0) {
+		cout << "\nПодстрока " << form << " не входит в строку " << line << endl;
+		cout << "-1";
+	}
+	else {
+		cout << "\nИндексы начала вхождений " << form << " в " << line << endl;
+		for (size_t i = 0; i < res.size(); i++) {
+			cout << res[i];
+			if (i != res.size() - 1) {
+				cout << ",";
+			}
+		}
+	}
+	return 0;
+}
---
+++ Syrtsova/lab 4/Source/PiAA4_2.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector <int> prefix(string arr) {//префикс функция 
+	cout << endl << "Вычислим префикс функцию строки" << endl;
+	vector <int> p(arr.size());
+	size_t j = 0;
+	size_t i = 1;
+	p[0] = 0;
+	cout << "Первое вхождение символа\nПрефикс " << arr[0] << " = 0" << endl;
+	while (i < arr.length()) {
+		if (arr[i] == arr[j]) {//если символы совпадают
+			cout << "Символ " << arr[i] << " совпадает с " << arr[j] << endl;
+			p[i] = j + 1;//увеличиваем префикс 
+			cout << "Префикс " << arr[i] << " = " << p[i] << endl;
+			i++;
+			j++;
+		}
+		else if (j == 0) {//еще не было совпадений
+			p[i] = 0;
+			cout << "Символ " << arr[i] << " не совпадает с " << arr[0] << endl;
+			cout << "Префикс " << arr[i] << " = " << p[i] << endl;
+			i++;
+		}
+		else {//символы не совпадают
+			cout << "Символ " << arr[i] << " не совпадает с " << arr[j] << endl;
+			j = p[j - 1];
+		}
+	}
+	cout << "Префикс функция строки ";
+	for (i = 0; i < arr.size(); i++) cout << p[i];
+	cout << endl;
+	return p;
+}
+
+int CYCLE_KMP(string s1, string s2) {
+	size_t i = 0;
+	size_t j = 0;
+	if (s1.length() != s2.length()) {//длины строк не равны
+		cout << "\nДлины строк не равны" << endl;
+		return -1;
+	}
+	else if (s1 == s2) {//строки равны
+		cout << "\nСтроки равны" << endl;
+		return 0;
+	}
+	else {//если нет то алгоритм кмп
+		vector <int> p = prefix(s2);
+		cout << "\nРассмотрим строки на циклический сдвиг" << endl;
+		while (true) {
+			if (s1[i] == s2[j]) {//символы совпадают
+				cout << "Символ " << s1[i] << " совпадает с " << s2[j] << endl;
+				i++;
+				if (i == s1.length()) {
+					i -= s1.length();
+				}
+				j++;
+				if (j == s2.length()) {
+					cout << "\nСтроки циклически сдвинуты" << endl;
+					cout << "Индекс начала строки " << s1 << " в " << s2 << endl;
+					return i;
+					//return s2.length() - i;
+				}
+			}
+			else if (j != 0) {//не совпадают, откатываем назад по префиксу
+				if (j != s2.length()) cout << "Символ " << s1[i] << " не совпадает с " << s2[j] << endl;
+				j = p[j - 1];
+				cout << "Вернемся к предыдущему символу префикса строки" << endl;
+			}
+			else {//не равны
+				cout << "Символы " << s1[i] << " и " << s2[j] << " не совпадают" << endl;
+				i++;//идем вперед
+				if (i == s1.length()) {//если строка закончилась 
+					cout << "\nСтроки не циклически сдвинуты" << endl;
+					return -1;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	string a;//строка 1
+	string b;//строка 2
+	getline(cin, a);
+	getline(cin, b);
+	cout << CYCLE_KMP(b, a);
+	return 0;
+}
